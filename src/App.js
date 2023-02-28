e50663de807525bb24b136f0e9b3778ab67e82ae
fhhdjfhedjdkjd123 <bash>
2023-02-28 16:36:40 +0530

    add restaurent summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
 import Header from './components/Layout/Header.js';
+import MealsSummary from './components/Meals/MealsSummary.js';
 
 function App() {
   return (
     <Fragment>
       <Header/>
+      <MealsSummary />
     </Fragment>
   );
 }
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './MealsSummary.module.css';
+const MealsSummary =()=>{
+    return(
+        <section className={classes.summary}>
+            <h2>Delicious Food,Delivered to you</h2>
+            <p>Choose ndcdnsdknsdlmds;ldsmfkdsjbdfksljds;'djdi2ege2ijef223e23uyger2eifkefklenc</p>
+            <p>hdgsyudswdjwdp pcsd bjhdisjdspodks pdos dskdjspodkwewewidjwdk wdsiudsodkspodksdshsdnsodkjssbudhwwqudjqdjqps</p>
+        </section>
+    )
+}
+export default MealsSummary;
