b966c66860d47af01fe10fb396b03121d68cfd0e
fhhdjfhedjdkjd123 <bash>
2023-02-28 16:51:43 +0530

    edited summary

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -5,7 +5,7 @@ const MealsSummary =()=>{
         <section className={classes.summary}>
             <h2>Delicious Food,Delivered to you</h2>
             <p>Choose your favourite meal from ourbrand selection of availabe meals and enjoy a delicious lunch or dinnar at home.</p>
-            <p>All our meals are cooked with high-quality ingrediants,just-in-time and of course by very experienced chefs!!</p>
+            <p>All our meals are cooked with high-quality ingrediants,just-in-time and of course by very experienced chefs!!!!!</p>
         </section>
     )
 }
